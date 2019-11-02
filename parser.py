50933516f57c0a98642e77d4a390dc120835578d
sergey <s.k1990@mail.ru>
2019-11-03 02:30:26 +0300

    2 launches

--- parser.py
+++ parser.py
@@ -7,11 +7,11 @@ URL = {'google': '', 'yandex': ''}
 pyppeteer.DEBUG = True
 
 
-async def main(ports=(9050, 9051)):
+async def parser(ports=(9050, 9051), title='example1.png'):
     browser = await launch(args=[f'--proxy-server=socks5://127.0.0.1:{ports[0]}', ])
     page = await browser.newPage()
     await page.goto('https://2ip.ru/')
-    await page.screenshot({'path': 'example.png'})
+    await page.screenshot({'path': title})
     await browser.close()
 
 
@@ -23,4 +23,8 @@ async def execute_requests_to_yandex():
     pass
 
 
-asyncio.get_event_loop().run_until_complete(main())
+async def main():
+    await asyncio.gather(parser(), parser(ports=(9111, 9112), title='example2.png'))
+
+# asyncio.get_event_loop().run_until_complete(main(), main((9065, 9066), 'example2.png'))
+asyncio.run(main())
