ed3c80b0f6c69a6695a62cfc7cd058ae8094127e
sergey <s.k1990@mail.ru>
2019-11-07 23:57:26 +0300

    Refactoring change_ip

--- connection.py
+++ connection.py
@@ -5,26 +5,24 @@ from aiohttp_socks import SocksConnector, SocksVer
 
 class Connection:
     def __init__(self, ports=(9050, 9051), host='127.0.0.1'):
-        self.port = ports[0]
-        self.control_port = ports[1]
+        self.port, self.control_port = ports
         self.host = host
         self.ip = None
 
     async def change_ip(self):
         while True:
             await self._send_signal_change_ip()
-            new_ip = await self._get_ip()
-            new_ip = new_ip.strip()
+            new_ip = await self.get_ip()
             if self.ip != new_ip:
                 self.ip = new_ip
                 break
-            await asyncio.sleep(1)
 
-    async def _get_ip(self):
+    async def get_ip(self):
         connector = SocksConnector(socks_ver=SocksVer.SOCKS5, host=self.host, port=self.port, rdns=True)
         async with ClientSession(connector=connector) as session:
             async with session.get('http://icanhazip.com/') as response:
-                return await response.text()
+                current_ip = await response.text()
+                return current_ip.rstrip()
 
     async def _send_signal_change_ip(self):
         _, writer = await asyncio.open_connection(self.host, self.control_port)
@@ -33,3 +31,4 @@ class Connection:
         await writer.drain()
         writer.close()
         await writer.wait_closed()
+        await asyncio.sleep(1)
--- parser.py
+++ parser.py
@@ -1,32 +1,41 @@
 import asyncio
 from pyppeteer import launch
 import pyppeteer
-
+from connection import Connection
+from search_query import QueryList
 
 URL = {'google': '', 'yandex': ''}
 pyppeteer.DEBUG = True
 
 
-async def parser(port=9050, host='127.0.0.1'):
-    browser = await launch(args=[f'--proxy-server=socks5://{host}:{port}', ])
-    page = await browser.newPage()
-    # await page.goto('https://2ip.ru/')
-    # await page.screenshot({'path': title})
-    # await browser.close()
+async def parser(connection, queue):
+    connection.ip = await connection.get_ip()
+    while not queue.empty():
+        query = queue.get()
+        browser = await launch(args=[f'--proxy-server=socks5://{connection.host}:{connection.port}', ])
+        page = await browser.newPage()
+        await execute_requests_to_google(page, query)
+        await execute_requests_to_yandex(page, query)
+        await browser.close()
+        await connection.change_ip()
 
 
-async def execute_requests_to_google():
-    pass
+async def execute_requests_to_google(page, query):
+    print('отработала execute_requests_to_google')
 
 
-async def execute_requests_to_yandex():
-    pass
+async def execute_requests_to_yandex(page, query):
+    print('отработала execute_requests_to_yandex')
 
 
 async def run_parsers():
     queue = asyncio.Queue()
-    await asyncio.gather(parser(), parser(port=9052))
+    for query in queries:
+        await queue.put(query)
+    await asyncio.gather(*[parser(connection, queue) for connection in connections])
 
 
 if __name__ == '__main__':
+    connections = [Connection(), ]
+    queries = QueryList('test.json')
     asyncio.run(run_parsers())
